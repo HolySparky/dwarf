2433363e60e54c2af342c03822f03bd7d6da86ba
HolySparky <lalasjtu@gmail.com>
2013-06-18 17:24:41 +0800

    added config files right, first just server ips

--- dwarf_agent.py
+++ dwarf_agent.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8:
 
+import ConfigParser
 import logging
 #import MySQLdb
 import os
@@ -9,9 +10,32 @@ import time
 
 import socket
 
+
 def main():
+    config_file = "agent.ini"
+    config = ConfigParser.ConfigParser()
+    try:
+        config.read(config_file)
+    except Exception, e:
+        print "Exception: Could not parse agent config file"
+        LOG.error("Unable to parse config file \"%s\": %s"
+                  % (config_file, str(e)))
+        raise e
+    server_ip = ""
+    server_port = ""
+
+    # Get Dwarf-server parameters
+    try:
+        server_ip = config.get("SERVER", "server_ip")
+        server_port = config.get("SERVER", "server_port")
+    except Exception, e:
+        pass
+
+    print "readed: server :" + server_ip + "and port: " + server_port
+
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-    sock.connect(('10.50.8.42', 8001))  
+    sock.connect((server_ip, int(server_port)))  
     sock.send('1')  
     print sock.recv(1024)  
     sock.close()  
