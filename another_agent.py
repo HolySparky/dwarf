49e97039da910206ba9bfb5a4673df98cf5abf98
HolySparky <lalasjtu@gmail.com>
2013-07-07 09:19:30 +0800

    merge dwarf.agent with another_agent

--- another_agent.py
+++ another_agent.py
@@ -9,6 +9,86 @@ import time
 
 import subprocess
 
+import socket
+import simplejson as json
+from optparse import OptionParser
+from sqlalchemy.ext.sqlsoup import SqlSoup
+
+OUT_PORT = 'eth1'
+ports = {}
+guarantees = {}
+
+class Flow_Info:
+    def __init__(self):
+        self.src_host = ""
+        self.dst_host = ""
+        self.src_ip = ""
+        self.dst_ip = ""
+
+    def set_host(self, src, dst):
+        self.src_host = src
+        self.dst_host = dst
+
+
+def PreConfigur():
+    global guarantees
+    #Reading the ini file to get both server connection and SQL connections
+    config_file = "agent.ini"
+    config = ConfigParser.ConfigParser()
+    try:
+        config.read(config_file)
+    except Exception, e:
+        print "Exception: Could not parse agent config file"
+        LOG.error("Unable to parse config file \"%s\": %s"
+                  % (config_file, str(e)))
+        raise e
+    server_ip = ""
+    server_port = ""
+
+    # Get Dwarf-server parameters and sql_url
+    try:
+        server_ip = config.get("SERVER", "server_ip")
+        server_port = config.get("SERVER", "server_port")
+	db_url = config.get("SQL", "sql_connection")
+    except Exception, e:
+        pass
+    print "readed: server :" + server_ip + "and port: " + server_port
+    print "db_url : " + db_url
+
+    # This is how agent could communicate with server
+    flow1 = Flow_Info()
+    flow1.set_host("mao", "la")
+    flow2 = {"src_host":"mao", "dst_host":"la"}
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+    sock.connect((server_ip, int(server_port)))  
+    sock.send(json.dumps(flow2))  
+    print sock.recv(1024)  
+    print flow2
+    print json.loads(json.dumps(flow2))
+    sock.close()  
+
+    #This is to get all guarantees from the sql in the dwarf-server
+    options = {"sql_connection": db_url}
+    db = SqlSoup(options["sql_connection"])
+    #LOG.info("Connecting to database \"%s\" on %s" %
+    port_g = db.port_guarantee.all()
+    db.commit()
+
+    for port in port_g:
+	    guarantees[port.port_name]={'0':port.guarantee}
+
+    print guarantees
+            
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
 OUT_PORT = 'eth1'
 ports = {}
 
