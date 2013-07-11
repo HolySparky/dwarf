ad94e9272a493887067c44f80bd082af9f699116
root <root@IBM-C5.(none)>
2013-07-11 17:50:31 +0800

    got messed up in c22, now roll back and try again in c5

--- dwarf_server.py
+++ dwarf_server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8:
-
+import ConfigParser
 import logging
 import MySQLdb
 import os
@@ -9,23 +9,49 @@ import time
 
 import socket
 import simplejson as json
+from optparse import OptionParser
+from sqlalchemy.ext.sqlsoup import SqlSoup
 
-class Flow_Info:
-    def __init__(self):
-        self.src_host = ""
-        self.dst_host = ""
-        self.src_ip = ""
-        self.dst_ip = ""
+ip_ports = {}
+server_ip = ""
+server_port = ""
+db_url = ""
 
-    def set_host(self, src, dst):
-        self.src_host = src
-        self.dst_host = dst
+def PreConfig():
+    global server_ip
+    global server_port
+    config_file = "agent.ini"
+    config = ConfigParser.ConfigParser()
+    try:
+    	config.read(config_file)
+    except Exception, e:
+	print "Exception: Could not parse agent config file"
+	LOG.error("Unable to parse config file \"%s\": %s"
+		% (config_file, str(e)))
+	raise e
 
+    try:
+        server_ip = config.get("SERVER", "server_ip")
+	server_port = config.get("SERVER", "server_port")
+	db_url = config.get("SQL", "sql_connection")
+    except Exception, e:
+	pass
+    print "readed: server :" + server_ip + "and port: " + server_port
+    print "db_url : " + db_url
+    
+    options = {"sql_connection": db_url}
+    db = SqlSoup(options["sql_connection"])
+    ips = db.ip_port.all()
+    db.commit()
+    for ip in ips:
+	ip_ports[ip.ip] = {"port":ip.port_name, "host":ip.host_ip}
+    print ip_ports
 
 
 def main():
+    PreConfig()
     sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('0.0.0.0', 8001))
+    sock.bind((server_ip, int(server_port)))
     sock.listen(100)
     while True:  
         connection,address = sock.accept()  
@@ -34,6 +60,8 @@ def main():
             buf = connection.recv(1024)  
             flow2 = json.loads(buf)
             print "Server Received: %s " %(flow2)
+#Server Received: {u'dst_host': u'la', u'src_host': u'mao'}
+	    print flow2["src_ip"]
         except socket.timeout:  
             print 'time out'  
         connection.close()   
